fe92003456245fd1281bf6aa652197b48481d75e
dannyblueman <56080498+dannyblueman@users.noreply.github.com>
2020-09-01 17:46:33 +0200

    Implemented PAIR and THREE_OF_A_KIND (#1)
    * Red test for PAIR
    
    * PAIRS implemented, new red test
    
    * Progress on PAIR
    
    * PAIRS green, added THREE_OF_A_KIND (red)
    
    * Complete THREE_OF_A_KIND
    
    * fixed small typo

--- src/test/java/no/kristiania/yatzy/YatzyTest.java
+++ src/test/java/no/kristiania/yatzy/YatzyTest.java
@@ -18,4 +18,17 @@ public class YatzyTest {
         assertEquals(2, yatzygame.score(YatzyCategory.TWOS, new int[] {1, 1, 2, 1, 1,}));
         assertEquals(4, yatzygame.score(YatzyCategory.TWOS, new int[] {6, 5, 2, 2, 1,}));
     }
+
+    @Test
+    void shouldScorePair(){
+        assertEquals(10, yatzygame.score(YatzyCategory.PAIR, new int[] {1, 3, 5, 5, 2}));
+        assertEquals(12, yatzygame.score(YatzyCategory.PAIR, new int[] {1, 6, 6, 5, 2}));
+        assertEquals(12, yatzygame.score(YatzyCategory.PAIR, new int[] {5, 5, 6, 6, 2}));
+    }
+
+    @Test
+    void shouldScoreThreeOfAKind(){
+        assertEquals(12, yatzygame.score(YatzyCategory.THREE_OF_A_KIND, new int[] {4, 4, 6, 4, 2}));
+        assertEquals(18, yatzygame.score(YatzyCategory.THREE_OF_A_KIND, new int[] {4, 6, 6, 4, 6}));
+    }
 }
